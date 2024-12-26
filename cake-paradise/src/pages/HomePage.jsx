3dfea9a7355b899b63fdbced41b8c1995f29a02b
Rashi990 <rashinilakshika2020@gmail.com>
2024-12-27 01:06:58 +0530

    HomeTopBanner done

--- cake-paradise/src/pages/HomePage.jsx
+++ cake-paradise/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../components/Header/Header';
-
+import HomeBannerTop from '../components/Home/HomeBannerTop';
 
 function HomePage() {
   return (
     <div style={{ background: "#ffe6e6", minHeight: "100vh", margin: "0" }}>
         <Header/>
-        
+        <HomeBannerTop/>
+
     </div>
   );
 }
