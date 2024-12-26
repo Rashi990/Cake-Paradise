ea95f5da39529d6415d4928985f0c94c026e5f72
Rashi990 <rashinilakshika2020@gmail.com>
2024-12-27 01:23:03 +0530

    Home Img Carousel done

--- cake-paradise/src/pages/HomePage.jsx
+++ cake-paradise/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header/Header';
+import HomeImgCarousel from '../components/Home/HomeImgCarousel';
 import HomeBannerTop from '../components/Home/HomeBannerTop';
 
 function HomePage() {
@@ -7,7 +9,8 @@ function HomePage() {
     <div style={{ background: "#ffe6e6", minHeight: "100vh", margin: "0" }}>
         <Header/>
         <HomeBannerTop/>
-
+        <HomeImgCarousel/>
+        <Footer/>
     </div>
   );
 }
